71900c0467730161a149d9942cdf51515f8fd4ec
barrosol <lucas.barroso@impactzero.pt>
2022-11-10 11:16:45 +0000

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by Lucas Barroso IZ on 10/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
